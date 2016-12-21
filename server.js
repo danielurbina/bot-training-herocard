4d27f222203c0b0b3570f7717ac8a77cab6f692e
danielurbina <danielu@microsoft.com>
2016-12-21 00:15:51 +0000

    Changed <b>

--- server.js
+++ server.js
@@ -153,7 +153,7 @@ bot.dialog('/cards', [
                 new builder.HeroCard(session)
                     .title("Hero Card")
                     .subtitle("Space Needle")
-                    .text("The _Space Needle_ is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
+                    .text("The <b>Space Needle<b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
                     .images([
                         builder.CardImage.create(session, 
                             "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
